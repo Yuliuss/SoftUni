ead3647ac4bd0ca67ef1f0bb135b68a15222c2f8
Yuliuss <82160571+Yuliuss@users.noreply.github.com>
2022-04-28 12:53:55 +0300

    Add files via upload

---
+++ C# Fundamentals/Lists - Exercise/_02.ChangeList/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.ChangeList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOfInt = Console.ReadLine()
+                   .Split()
+                   .Select(int.Parse)
+                   .ToList();
+
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                string[] cmdArgs = command.Split();
+                string firstCommand = cmdArgs[0];
+                int element = int.Parse(cmdArgs[1]);
+
+                if (firstCommand == "Delete")
+                {
+                    listOfInt.RemoveAll(x => x == element);
+                }
+                else if (firstCommand == "Insert")
+                {
+                    int index = int.Parse(cmdArgs[2]);
+                    listOfInt.Insert(index, element);
+                }
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", listOfInt));
+        }
+    }
+}
---
+++ C# Fundamentals/Lists - Lab/_05.RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.RemoveNegativesAndReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> sequence = Console.ReadLine().Split(' ').Select(int.Parse).Reverse().ToList();
+            List<int> sequence2 = new List<int>();
+            foreach (var num in sequence)
+            {
+                if (num > 0)
+                {
+                    sequence2.Add(num);
+                }
+            }
+            if (sequence2.Count < 1)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", sequence2));
+            }
+        }
+    }
+}
