13b5e30ff8b2c0c5a47e6fb2151536fddc401b30
Yuliuss <82160571+Yuliuss@users.noreply.github.com>
2022-04-28 13:26:00 +0300

    Add files via upload

---
+++ C# Advanced/Iterators and Comparators - Exercise/02. Collection/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListyIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> items = Console.ReadLine().Split(" ").Skip(1).ToList();
+            ListyIterator<string> list = new ListyIterator<string>(items);
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                switch (command)
+                {
+                    case "Move":
+                        Console.WriteLine(list.Move());
+                            break;
+                    case "HasNext":
+                        Console.WriteLine(list.HasNext());
+                            break;
+                    case "Print":
+                        try
+                        {
+                            list.Print();
+                        }
+                        catch (InvalidOperationException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    case "PrintAll":
+                        //foreach (string item in list)
+                        //{
+                        //    Console.WriteLine(item);
+                        //}
+                        Console.WriteLine(string.Join(" ", list));
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ C# Advanced/Iterators and Comparators - Lab/04. BookComparer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IteratorsAndComparators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bookOne = new Book("Animal Farm", 2003, "George Orwell");
+            var bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustance");
+            var bookThree = new Book("The Documents in the Case", 1930);
+
+            var libraryOne = new Library();
+            var libraryTwo = new Library(bookOne, bookTwo, bookThree);
+        }
+    }
+}
