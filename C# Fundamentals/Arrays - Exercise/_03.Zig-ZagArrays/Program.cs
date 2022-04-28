5a3d6ec0d728f1a3d44964d7f1d05412e5344345
Yuliuss <82160571+Yuliuss@users.noreply.github.com>
2022-04-28 12:49:03 +0300

    Add files via upload

---
+++ C# Fundamentals/Arrays - Exercise/_03.Zig-ZagArrays/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Zig_ZagArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] firstArray = new string[n];
+            string[] secondtArray = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] currentArray = Console.ReadLine()
+                    .Split(" ")
+                    .ToArray();
+                string indexZeroElement = currentArray[0];
+                string indexOneElement = currentArray[1];
+
+                if (i % 2 == 0)
+                {
+                    firstArray[i] = indexZeroElement;
+                    secondtArray[i] = indexOneElement;
+                }
+                else if (i % 2 == 1)
+                {
+                    firstArray[i] = indexOneElement;
+                    secondtArray[i] = indexZeroElement;
+                }
+            }
+            Console.WriteLine(string.Join(" ", firstArray));
+            Console.WriteLine(string.Join(" ", secondtArray));
+        }
+    }
+}
---
+++ C# Fundamentals/Arrays - Lab/_08.CondenseArrayToNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _08.CondenseArrayToNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(' ');
+            int[] array = new int[numbers.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = int.Parse(numbers[i]);
+            }
+
+            int[] condensed = new int[array.Length - 1];
+
+            while (array.Length > 1)
+            {
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    condensed[i] = array[i] + array[i + 1];
+                    array[i] = condensed[i];
+                }
+                Array.Resize(ref condensed, condensed.Length - 1);
+                Array.Resize(ref array, array.Length - 1);
+            }
+
+            foreach (var element in array)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
