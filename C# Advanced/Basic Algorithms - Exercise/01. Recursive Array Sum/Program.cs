cf365fc61bd636289b5d6421af4e61f3bdc9f657
Yuliuss <82160571+Yuliuss@users.noreply.github.com>
2022-04-28 13:30:59 +0300

    Add files via upload

---
+++ C# Advanced/Basic Algorithms - Exercise/01. Recursive Array Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Recursive_Array_Sum
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            long sum = Sum(numbers, 0);
+            Console.WriteLine(sum);
+        }
+        public static long Sum(int[] arr, int startIndex)
+        {
+            if (startIndex == arr.Length - 1)
+            {
+
+                return arr[startIndex];
+            }
+            return arr[startIndex] + Sum(arr, startIndex + 1);
+        }
+    }
+}
---
+++ C# Advanced/Basic Algorithms - Exercise/02. Recursive Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02._Recursive_Factorial
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long fact = Factorial(n);
+
+            Console.WriteLine(fact);
+        }
+        public static long Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * Factorial(n - 1);
+        }
+    }
+}
