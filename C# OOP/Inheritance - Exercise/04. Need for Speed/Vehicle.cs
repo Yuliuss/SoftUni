21ba19f9e64a0e2b11ea1f75a0317bc3084aa8dc
Yuliuss <82160571+Yuliuss@users.noreply.github.com>
2022-04-28 18:14:56 +0300

    Add files via upload

---
+++ C# OOP/Inheritance - Exercise/04. Need for Speed/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private const double DefaulFuelConsumption = 1.25;
+        public Vehicle(int horsePower, double fuel)
+        {
+            this.Fuel = fuel;
+            this.HorsePower = horsePower;
+        }
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
+        public virtual double FuelConsumption => DefaulFuelConsumption;
+
+        public virtual void Drive (double kilometers)
+        {
+            this.Fuel -= kilometers * this.FuelConsumption;
+        }
+    }
+}
---
+++ C# OOP/Inheritance - Lab/02. Multiple Inheritance/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Puppy sharo = new Puppy();
+            sharo.Eat();
+            sharo.Bark();
+            sharo.Weep();
+        }
+    }
+}
